41cf47321005e12bf680351c1ddd47d9de5cb912
Rita Nguema <marvshcherbakova@gmail.com>
2024-01-20 19:14:48 +0300

    app layout

---
+++ src/components/HomePage.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class ='main'>
+        <h1>EXPLORE THE WORLD <br/> WITH US!</h1> 
+        <h2>Travel Blog</h2>
+        <button>Our posts</button>
+    </div>
+
+    <PostsListPage/>
+    <SinglePostView/>
+</template>
+
+<script>
+import PostsListPage from './PostsListPage.vue';
+import SinglePostView from './SinglePostView.vue';
+
+
+export default {
+    components: {
+        PostsListPage,
+        SinglePostView
+    }
+}
+</script>
+
+<style scoped>
+.main{
+    background-image: url(src/assets/img/cover-main.jpg);
+    background-size: cover;
+    padding-top: 250px;
+    padding-bottom: 100px;
+}
+
+.main h1{
+    text-align: center;  
+    text-shadow: 2px 2px 2px rgb(87, 83, 83);
+    font-size:40px;
+}
+
+.main h2{
+    text-align: center;  
+    padding-top: -15px;
+    color: rgba(0, 0, 0, 0.717);
+}
+
+.main button{
+    cursor: pointer;
+}
+
+</style>
